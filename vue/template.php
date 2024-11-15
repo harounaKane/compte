25a61880cc00127cff465e92b49113f6f98885d0
IdrisHAMMI <hammi.idris@gmail.com>
2024-11-15 12:15:51 +0100

    modified template title

--- vue/template.php
+++ vue/template.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css">
-    <title>Compte bancaire</title>
+    <title>Bank Account</title>
 </head>
 <body>
 
